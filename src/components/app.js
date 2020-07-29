ad78d5e5eb0a187aed5ce4b75b7c53b0a5d56829
kevincastrochavez <kevin.castro.chavez0@gmail.com>
2020-07-29 16:21:25 -0500

    first commit

---
+++ src/components/app.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { JournalList } from './journals/journal_list.js';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <h1>This, Props and State</h1>
+      </div>
+    );
+  }
+}
---
+++ src/components/journals/journal_list.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+const rawJournalData = [
+    { title: 'Post One', content: 'Post Content', status: 'draft'},
+    { title: 'Post Two', content: 'Post Content', status: 'published'},
+    { title: 'Post Three', content: 'Post Content', status: 'published'},
+    { title: 'Post Four', content: 'Post Content', status: 'published'}
+];
+
+export default class JournalList extends Component {
+   render() {
+       return (
+           <div>
+               
+           </div>
+       );
+   };
+}
